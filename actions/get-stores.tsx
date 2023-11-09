79f5e194823e5c2303844fed22205c2f921a2e9b
Waldo Sobrino <waldo2810@gmail.com>
2023-11-08 20:38:20 -0500

    store owner env var

--- actions/get-stores.tsx
+++ actions/get-stores.tsx
@@ -4,7 +4,7 @@ import axios from "axios";
 const URL = `${process.env.NEXT_PUBLIC_API_URL}/store`;
 
 export const getStores = async () => {
-  const userEmail = "waldo2810@gmail.com";
+  const userEmail = process.env.NEXT_PUBLIC_STORE_OWNER;
 
   try {
     const { data } = await axios.get(URL, {
